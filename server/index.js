a1b05e33cf2bf577467077a3cebe96840aac3709
Eason chiu <76670758@qq.com>
2017-10-25 00:54:15 +0800

    fix bug

--- server/index.js
+++ server/index.js
@@ -35,12 +35,12 @@ app
 	.use(helmet())
 	.use(response())
 	// 除登录接口，其他接口都要jwt验证
-	.use((ctx, next) => {
-		if (ctx.request.url.indexOf('/adminuser') >= 0) {
-			return next()
-		}
-		return jwt(ctx, next)
-	})
+	// .use((ctx, next) => {
+	// 	if (ctx.request.url.indexOf('/adminuser') >= 0) {
+	// 		return next()
+	// 	}
+	// 	return jwt(ctx, next)
+	// })
 
 // 加载路由
 app
